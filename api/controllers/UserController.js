1711eb37f407c7ba920a03838d9fdcf6b86cee3e
Dave Albert <dave.albert@publicisdhealthcare.ie>
2014-03-09 18:45:21 +0000

    building-a-sails-application-ep9-deleting-a-user-account

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -62,7 +62,7 @@ module.exports = {
 
         User.findOne(req.param('id'), function foundUser(err, user){
             if (err) { return next(err); }
-            if (!user) { return next(); }
+            if (!user) { return next("User doesn't exist."); }
 
             res.view({
                 user: user
@@ -77,6 +77,21 @@ module.exports = {
             }
 
             res.redirect('/user/show/' + req.param('id'));
+        });
+    },
+
+    destroy: function(req, res, next){
+
+        User.findOne(req.param('id'), function foundUser(err, user){
+            if (err) { return next(err); }
+            if (!user) { return next("User doesn't exist."); }
+
+            User.destroy(req.param('id'), function userDestroyed (err) {
+               if (err) { return next(err); }
+            });
+
+            res.redirect('/user');
+
         });
     }
 
