f257991bf033e48dc2c23cc2527b8289a42a394f
Dave Albert <dave.albert@publicisdhealthcare.ie>
2014-03-09 11:18:47 +0000

    building-a-sails-application-ep4-handling-validation-errors-with-a-flash-message

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -17,14 +17,26 @@
 
 module.exports = {
     'new': function  (req, res) {
+       res.locals.flash = _.clone(req.session.flash);
        res.view();
+       req.session.flash = {};
     },
 
-    create: function (req, res) {
+    create: function (req, res, next) {
         User.create( req.params.all(), function userCreated (err, user) {
-            if (err) { return next(err); }
+            //if (err) { return next(err); }
+            if (err) {
+                console.log(err);
+
+                req.session.flash = {
+                    err: err
+                }
+
+                return res.redirect('/user/new');
+            }
 
             res.json(user);
+            req.session.flash = {};
         });
     }
 
